1cda62bc8891c879c3540793d6b2f8ef71c22455
Allaye <kolade199@gmail.com>
2021-01-14 11:28:02 +0000

    added images

--- github.js
+++ github.js
@@ -1,7 +1,7 @@
 class RepoGetter{
     constructor(name){
-        this.client_id = 'myclientid';
-        this.client_secret = 'myclientkey';
+        this.client_id = '3ed08bf3def1411626e9';
+        this.client_secret = '3af67041cb3a04c975ef647038fecbd7a16c15b9';
         this.repo_count = 5;
         this.repo_display = 'created: asc';
     }
@@ -13,6 +13,7 @@ class RepoGetter{
 
         const profile = await profileResponse.json();
         const repos = await repoResponse.json();
+
 
         return {profile: profile, repos: repos};
     }
